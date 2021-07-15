1103604d0cf87cf2e86f7f9730530e42e71832cf
Guilherme Rigotti <ggrigotti@gmail.com>
2021-07-14 21:36:48 -0300

    chapter05 - operator add ages

--- chapter05/operators.go
+++ chapter05/operators.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Operator interface {
 	Calculate() int
@@ -30,6 +33,18 @@ func (s Subtract) String() string {
 	return fmt.Sprintf("%d - %d", s.operator1, s.operator2)
 }
 
+type Age struct {
+	birthYear int
+}
+
+func (i Age) Calculate() int {
+	return time.Now().Year() - i.birthYear
+}
+
+func (i Age) String() string {
+	return fmt.Sprintf("Birth year %d", i.birthYear)
+}
+
 func main() {
 	operators := make([]Operator, 4)
 
@@ -38,6 +53,17 @@ func main() {
 	operators[2] = Subtract{30, 15}
 	operators[3] = Subtract{25, 5}
 
+	fmt.Println("Accumulate values: ", accumulate(operators))
+
+	ages := make([]Operator, 3)
+	ages[0] = Age{1999}
+	ages[1] = Age{2000}
+	ages[2] = Age{2001}
+
+	fmt.Println("Accumulate age values: ", accumulate(ages))
+}
+
+func accumulate(operators []Operator) int {
 	accumulator := 0
 
 	for _, op := range operators {
@@ -46,5 +72,5 @@ func main() {
 		accumulator += value
 	}
 
-	fmt.Println("Accumulator: ", accumulator)
+	return accumulator
 }
